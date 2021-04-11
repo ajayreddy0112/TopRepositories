df38ca055e2605f3ce32c0b8585c856cb7ca092a
Ajay Reddy <ajay.reddy@flexiloans.com>
2021-04-11 17:02:31 +0530

    route created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { GithubSearchComponent } from './components/github-search/github-search.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'githubSearch', component: GithubSearchComponent},
+  { path: '', redirectTo: 'githubSearch', pathMatch: 'full' },
+  { path: '**', redirectTo: 'githubSearch', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
